24da9398213cb8b43ffc15c52a31a63274f3e65b
111161226 <fanrin9@gmail.com>
2023-06-11 03:31:28 +0900

    fix db enviroment

--- docker/db/sql/init.sql
+++ docker/db/sql/init.sql
@@ -2,14 +2,14 @@
 DROP TABLE IF EXISTS `images`;
 
 CREATE TABLE `images` (
-    `image_id` int NOT NULL AUTO_INCREMENT,
-    `image_name` varchar(256) NOT NULL,
-    `image_type` varchar(64) NOT NULL,
-    `image_content` MEDIUMBLOB,
-    `image_size` int DEFAULT 0,
-    `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    PRIMARY KEY (`image_id`)
-) DEFAULT CHARSET=utf8mb4;
+  `image_id` int NOT NULL AUTO_INCREMENT,
+  `image_name` varchar(256) NOT NULL,
+  `image_type` varchar(64) NOT NULL,
+  `image_content` MEDIUMBLOB,
+  `image_size` int DEFAULT 0,
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`image_id`)
+) DEFAULT CHARSET=utf8mb4; 
 
 /* table for user*/
 DROP TABLE IF EXISTS `users`;
@@ -21,28 +21,28 @@ CREATE TABLE `users` (
   `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`user_id`),
   UNIQUE KEY `user_name` (`user_name`)
-); DEFAULT CHARSET=utf8mb4;
+) DEFAULT CHARSET=utf8mb4;
 
 /* table for category*/
 DROP TABLE IF EXISTS `category_list`;
 
-CREATE TABLE category_list (
-  `c_id` INT PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE `category_list` (
+  `c_id` int NOT NULL AUTO_INCREMENT,
   `category` VARCHAR(100) NOT NULL,
   UNIQUE KEY `c_id` (`c_id`),
-  PRIMARY KEY `c_id`
-); DEFAULT CHARSET=utf8mb4;
+  PRIMARY KEY (`c_id`)
+) DEFAULT CHARSET=utf8mb4;
 
 /* table for blog*/
 DROP TABLE IF EXISTS `blog`;
 
-CREATE TABLE blog (
+CREATE TABLE `blog` (
   `blog_id` VARCHAR(23) NOT NULL,
   `title` VARCHAR(100) NOT NULL,
   `text` TEXT NOT NULL,
   `date` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `thumnail` MEDIUMBLOB,
-  `c_id` INT NOT NULL,
+  `c_id` int NOT NULL,
   UNIQUE KEY `blog_id` (`blog_id`),
-  PRIMARY KEY `blog_id`
-); DEFAULT CHARSET=utf8mb4;
+  PRIMARY KEY (`blog_id`)
+) DEFAULT CHARSET=utf8mb4;
