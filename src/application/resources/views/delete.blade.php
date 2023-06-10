fd55c8e622d748927e85833428fb2be727cc7e8d
111161226 <fanrin9@gmail.com>
2023-06-11 03:50:38 +0900

    image delete process

---
+++ src/application/resources/views/delete.blade.php
@@ -0,0 +1,13 @@
+@include('functions')
+<?php
+
+$pdo = connectDB();
+
+$sql = 'DELETE FROM images WHERE image_id = :image_id';
+$stmt = $pdo->prepare($sql);
+$stmt->bindValue(':image_id', (int)$_GET['id'], PDO::PARAM_INT);
+$stmt->execute();
+
+header('Location:/home');
+exit();
+?>
