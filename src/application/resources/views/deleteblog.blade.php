4008b50dd924908f967962c3929a291e04ceaf9c
111161226 <fanrin9@gmail.com>
2023-06-14 00:12:08 +0900

    add remove thumnail in image

--- src/application/resources/views/deleteblog.blade.php
+++ src/application/resources/views/deleteblog.blade.php
@@ -3,10 +3,27 @@
 
 $pdo = connectDB();
 
-$sql = 'DELETE FROM blogs WHERE blog_id = :blog_id';
-$stmt = $pdo->prepare($sql);
-$stmt->bindValue(':blog_id', $_GET['id'], PDO::PARAM_STR);
-$stmt->execute();
+//delete thumnail
+try{
+    $sql = 'DELETE images FROM images INNER JOIN blogs ON image_id = thumnail_id WHERE blog_id = :blog_id';
+    $stmt = $pdo->prepare($sql);
+    $stmt->bindValue(':blog_id', $_GET['id'], PDO::PARAM_STR);
+    $stmt->execute();
+} catch(Exception $error){
+    echo "failed to delete thumnail" . $error->getMessage();
+    exit();
+}
+
+//delete blog
+try{
+    $sql = 'DELETE FROM blogs WHERE blog_id = :blog_id';
+    $stmt = $pdo->prepare($sql);
+    $stmt->bindValue(':blog_id', $_GET['id'], PDO::PARAM_STR);
+    $stmt->execute();
+} catch(Exception $error){
+    echo "failed to delete blog" . $error->getMessage();
+    exit();
+}
 
 header('Location:/lblog');
 exit();
