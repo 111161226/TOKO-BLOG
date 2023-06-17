c9dfa57a5748e6fd7bd2f9cb664dcad64e71cdaf
111161226 <fanrin9@gmail.com>
2023-06-17 23:40:39 +0900

    create sidevar

---
+++ src/application/resources/views/sidebar.blade.php
@@ -0,0 +1,31 @@
+<nav class="bg-dark">
+    <ul class="nav flex-column m-0 p-3">
+        <li class="nav-item mb-2"><a href="#" class="nav-link">Python</a></li>
+        <li class="nav-item mb-2"><a href="#" class="nav-link">Django</a></li>
+    </ul>
+</nav>
+<style type="text/css">
+    .container {
+        display: flex;
+        width: auto;
+        max-width: none;
+        height: auto;
+        max-height: none;
+        margin:0;
+        padding: 0;
+        overflow-y: scroll;
+    }
+    .sidebar {
+        width: 20%;
+        height: 100vh;
+        background-color: green;
+        overflow-y: scroll;
+    }
+    .body {
+        padding: 10px;
+        width: 80%;
+        height: 100vh;
+        overflow-y: scroll;
+        background-color: aqua;
+    }
+</style>
