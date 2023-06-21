22c28f696876ff37bf757dd9ee01eb6ba5402199
111161226 <fanrin9@gmail.com>
2023-06-21 17:16:31 +0900

    remove own blog from search result

--- src/application/resources/views/searchblog.blade.php
+++ src/application/resources/views/searchblog.blade.php
@@ -7,8 +7,10 @@
             //get all blogs from db
             try {
                 $sql = "SELECT blog_id, title, content, category, thumnail_id FROM `blogs` INNER JOIN `category_list` ON blogs.`c_id` = `category_list`.`c_id`  
-                        WHERE title LIKE '%".$_POST["title"]."%' AND category LIKE '%".$_POST["category"]."%' ";
+                        WHERE title LIKE '%".$_POST["title"]."%' AND category LIKE '%".$_POST["category"]."%' AND not exists (
+                            SELECT * from blog_owner WHERE author_id = :user_id AND blog_id = b_id) ORDER BY `created_at` DESC";
                 $stmt = $pdo->prepare($sql);
+                $stmt->bindValue(':user_id', $_SESSION['id'], PDO::PARAM_STR);
                 $stmt->execute();
                 $blogs = $stmt->fetchAll();
             } catch (Exception $error) {
@@ -26,6 +28,12 @@
     <title>Search form</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+    <style type="text/css">
+        #head {
+            text-align : center;
+            background-color:#1e93c1;
+        }
+    </style>
 </head>
 <body>
 <div class="container">
@@ -33,13 +41,14 @@
         @include('sidebar')  
     </div>
     <div class="body">
+        <h1 id="head">ブログ検索</h1>
         <form method="post">
             @csrf
             <p class="text-center">
             <br>
             <label>タイトル&nbsp;</label> <input type="text" name="title">
             <label>&nbsp;カテゴリー&nbsp;</label> <input type="text" name="category"> &nbsp; 
-            <input type="submit" name="submit" value="送信">
+            <input type="submit" name="submit" value="検索">
             </p>
         </form>
         <div class="col-md-8 border-right">
@@ -64,6 +73,38 @@
             </div>
         </div>
     </div>
+</div>
+
+<!-- show image ver Enlarge -->
+<div class="modal carousel slide" id="lightbox" tabindex="-1" role="dialog" data-ride="carousel">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-body">
+        <ol class="carousel-indicators">
+            <?php for ($i = 0; $i < count($blogs); $i++): ?>
+                <li data-target="#lightbox" data-slide-to="<?= $i; ?>" <?php if ($i == 0) echo 'class="active"'; ?>></li>
+            <?php endfor; ?>
+        </ol>
+
+        <div class="carousel-inner">
+            <?php for ($i = 0; $i < count($blogs); $i++): ?>
+                <div class="carousel-item <?php if ($i == 0) echo 'active'; ?>">
+                <img src="image?id=<?= $blogs[$i]['thumnail_id']; ?>" class="d-block w-100">
+                </div>
+            <?php endfor; ?>
+        </div>
+
+        <a class="carousel-control-prev" href="#lightbox" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#lightbox" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+      </div>
+    </div>
+  </div>
 </div>
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
