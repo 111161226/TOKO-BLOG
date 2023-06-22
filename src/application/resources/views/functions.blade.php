3834a83a78387ee6be34431a226e323343f2a4e3
111161226 <fanrin9@gmail.com>
2023-06-22 18:02:30 +0900

    add function to get username

--- src/application/resources/views/functions.blade.php
+++ src/application/resources/views/functions.blade.php
@@ -1,13 +1,31 @@
 <?php
-// connect database
-function connectDB() {
-    $param = 'mysql:dbname=mysql;host=mysql';
-    try {
-        $pdo = new PDO($param, "test", "test");
-        return $pdo;
+    // connect database
+    function connectDB() {
+        $param = 'mysql:dbname=mysql;host=mysql';
+        try {
+            $pdo = new PDO($param, "test", "test");
+            return $pdo;
 
-    } catch (PDOException $e) {
-        exit($e->getMessage());
+        } catch (PDOException $e) {
+            exit($e->getMessage());
+        }
     }
-}
+
+    //get user name
+    function getname($id) {
+        $pdo = connectDB();
+        //get user info
+        try {
+            $sql = "SELECT * FROM users WHERE user_id = :id";
+            $stmt = $pdo->prepare($sql);
+            $stmt->bindValue(':id', $id, PDO::PARAM_STR);
+            $stmt->execute();
+            $userinfo = $stmt->fetch();
+        } catch(Exception $error){
+            echo "failed to login" . $error->getMessage();
+            exit();
+        }
+
+        return $userinfo['user_name'];
+    }
 ?>
