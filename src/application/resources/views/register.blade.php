19832df2f4395c506b74522a22debc2d7b17e1e5
111161226 <fanrin9@gmail.com>
2023-06-23 11:42:26 +0900

    add thumnail to signup

--- src/application/resources/views/register.blade.php
+++ src/application/resources/views/register.blade.php
@@ -3,8 +3,46 @@
 //assign data from form
 $name = $_POST['username'];
 $pass = password_hash($_POST['pass'], PASSWORD_DEFAULT);
+$userId = uniqid('',true);
 
 $pdo = connectDB();
+
+$thumnail = $_FILES['thumnail'];
+$t_name = $thumnail['name'];
+$type = $thumnail['type'];
+$content = file_get_contents($thumnail['tmp_name']);
+$size = $thumnail['size'];
+$err_msg = '';
+
+//check file type and size(2M)
+$maxFileSize = 1048576*2;
+$validFileTypes = ['image/png', 'image/jpeg'];
+if ($size > $maxFileSize || !in_array($type, $validFileTypes)) {
+    $err_msg = 'please select * jpg, jpeg, png file up to 2MB';
+    echo $err_msg;
+    exit();
+}
+
+//insert thumnail into db
+if ($err_msg == '') {
+    //check error
+    try {
+        $sql = 'INSERT INTO user_thumnail (u_id, image_id, image_name, image_type, image_content, image_size, updated_at)
+                VALUES (:user_id, :image_id, :image_name, :image_type, :image_content, :image_size, now())';
+        $stmt = $pdo->prepare($sql);
+        $tid = uniqid('',true);
+        $stmt->bindValue(':user_id', $userId, PDO::PARAM_STR);
+        $stmt->bindValue(':image_id', $tid, PDO::PARAM_STR);
+        $stmt->bindValue(':image_name', $t_name, PDO::PARAM_STR);
+        $stmt->bindValue(':image_type', $type, PDO::PARAM_STR);
+        $stmt->bindValue(':image_content', $content, PDO::PARAM_STR);
+        $stmt->bindValue(':image_size', $size, PDO::PARAM_INT);
+        $stmt->execute();
+    } catch(Exception $error){
+        echo "failed to upload thumnail" . $error->getMessage();
+        exit();
+    }
+}
 
 //check username has already been used
 try {
@@ -28,7 +66,6 @@ else {
     try { 
         $sql = "INSERT INTO users(user_id, user_name, password) VALUES (:userId, :name, :pass)";
         $stmt = $pdo->prepare($sql);
-        $userId = uniqid('',true);
         $stmt->bindValue(':userId', $userId);
         $stmt->bindValue(':name', $name);
         $stmt->bindValue(':pass', $pass);
--- src/application/resources/views/signup.blade.php
+++ src/application/resources/views/signup.blade.php
@@ -9,12 +9,13 @@
     <title>Login form</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+    <style type="text/css"> #head {text-align : center;} </style>
 </head>
 <body>
 <div class="container">
     <div class="bg-info d-flex align-items-center justify-content-center" height="auto">
-        <form method="post">
-        <h1>登録ページ</h1>
+        <form method="post" enctype="multipart/form-data">
+        <h1 id="head">登録ページ</h1>
         <br>
         @csrf   
         <div>
@@ -29,6 +30,14 @@
                 <input type="password" name="pass" pattern="^[a-zA-Z0-9]+$" minlength="8" maxlength="30" required>
             </label>
         </div>
+        <div>
+            <p>
+                サムネイル：
+                <input name="thumnail" type="file" accept=".jpg,.jpeg,.png" required onchange="previewImage(this);">
+            <p class="text-center">
+            <img id="preview" src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==" style="max-width:200px;">
+            </p>
+        </div>
         <input type="submit" class="btn btn-primary" value="新規登録">
         <?php if ($err_msg != ''): ?>
             <div class="invalid-feedback d-block"><?= $err_msg; ?></div>
@@ -39,6 +48,16 @@
     </div>
 </div>
 
+<script>
+function previewImage(obj)
+{
+	var fileReader = new FileReader();
+	fileReader.onload = (function() {
+		document.getElementById('preview').src = fileReader.result;
+	});
+	fileReader.readAsDataURL(obj.files[0]);
+}
+</script>
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </body>
